be4f7702dcf246f9957396ae0a489d60408ddfd4
Udayraj Mandke <urmandke@gmail.com>
2020-08-13 10:49:26 +0530

    First commit for Aug 13 2020

---
+++ mathOperators.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	int x =  2 + 3 * 4/(3 - 2); // operator precidence -- brackets,  multiplication & division, subtraction & addition  
+	int y = 5 % 2; //modulas operator - remainder (interger)
+	return 0;
+}
