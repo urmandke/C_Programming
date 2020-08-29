5b54d7c5d5ebe8b2cc2684b477946e61579b1ab4
Udayraj Mandke <urmandke@gmail.com>
2020-08-29 12:44:21 +0530

    For Loops

--- Loops.c
+++ Loops.c
@@ -2,15 +2,12 @@
 
 int main()
 {
-	//inilization 
-	//comparision
-	//update
 
 	for (int i=0; i<10; i++)
 	{
 		printf("%d \n", i);
 	}
-	
+
 	return 0;
 }
 
---
+++ Loops_WithUserInput.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	//inilization 
+	//comparision
+	//update
+	int max;
+	scanf("%d", &max);
+	
+	getchar();
+	for (int i=0; i< max+1; i++)
+	{
+		printf("%d \n", i);
+	}
+
+	return 0;
+}
+
